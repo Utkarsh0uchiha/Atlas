6d0e2d7d3d0565881cab1679e842ba32de4406e4
Utkarsh0uchiha <uraj81922@gmail.com>
2026-06-17 21:18:31 +0530

    feat: implement backend abstraction

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-	"net/http"
-	"net/http/httputil"
+	"fmt"
 	"net/url"
 )
 
+type Backend struct {
+	URL *url.URL
+	Alive bool
+}
+
 func main() {
-	target, _ := url.Parse("http://localhost:8081")
+	backend1, err := url.Parse("http://localhost:8081")
+	if err != nil {
+		panic(err)
+	}
+	backend2, err := url.Parse("http://localhost:8082")
+	if err != nil {
+		panic(err)
+	}
 
-	proxy := httputil.NewSingleHostReverseProxy(target)
-
-	http.ListenAndServe(":8080", proxy)
+	backends := []Backend{
+		{URL: backend1, Alive: true},
+		{URL: backend2, Alive: true},
+	}
+	
+	for _, backend := range backends {
+		fmt.Println(backend.URL.String(), backend.Alive)
+	}
+	
 }
